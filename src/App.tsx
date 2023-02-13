e8a5e9f939df7420f756f1fd2a2f7b191c652b4b
bmstyojaop <bmbdyojaop@gmail.com>
2023-02-13 23:26:50 +0900

    :sparkles: 仮のカードコンポーネントを実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,54 @@ import React, { FC } from "react"
 import { Header } from "./components/Header"
 import { Card } from "./components/Card"
 
+const icons = [
+  { icon: "/assets/img/testicon1.jpg" },
+  { icon: "/assets/img/testicon2.jpg" },
+  { icon: "/assets/img/testicon3.jpg" },
+  { icon: "/assets/img/testicon4.jpg" },
+  { icon: "/assets/img/testicon5.jpg" },
+  { icon: "/assets/img/testicon6.jpg" },
+  { icon: "/assets/img/testicon7.jpg" },
+  { icon: "/assets/img/testicon8.jpg" },
+  { icon: "/assets/img/testicon9.jpg" },
+  { icon: "/assets/img/testicon10.jpg" },
+  { icon: "/assets/img/testicon1.jpg" },
+  { icon: "/assets/img/testicon2.jpg" },
+  { icon: "/assets/img/testicon3.jpg" },
+  { icon: "/assets/img/testicon4.jpg" },
+  { icon: "/assets/img/testicon5.jpg" },
+  { icon: "/assets/img/testicon6.jpg" },
+  { icon: "/assets/img/testicon7.jpg" },
+  { icon: "/assets/img/testicon8.jpg" },
+  { icon: "/assets/img/testicon9.jpg" },
+  { icon: "/assets/img/testicon10.jpg" },
+  { icon: "/assets/img/testicon1.jpg" },
+  { icon: "/assets/img/testicon2.jpg" },
+  { icon: "/assets/img/testicon3.jpg" },
+  { icon: "/assets/img/testicon4.jpg" },
+  { icon: "/assets/img/testicon5.jpg" },
+  { icon: "/assets/img/testicon6.jpg" },
+  { icon: "/assets/img/testicon7.jpg" },
+  { icon: "/assets/img/testicon8.jpg" },
+  { icon: "/assets/img/testicon9.jpg" },
+  { icon: "/assets/img/testicon10.jpg" },
+]
+
 const App: FC = () => {
   return (
     <div>
       <Header />
-      <main className="bg-gray-200 w-screen h-screen flex justify-center">
-        <div className="container flex flex-col items-center pt-4 md:flex-row md:items-start md:justify-around md:px-4 md:flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+      <main
+        className="bg-gray-200 w-screen 
+        h-screen 
+        flex 
+        justify-center
+      "
+      >
+        <div className="container flex flex-col items-center pt-4 md:px-4 md:flex-row md:items-start md:justify-around  md:flex-wrap">
+          {icons.map((icon) => (
+            <Card key={icon.icon} iconPath={icon.icon} />
+          ))}
         </div>
       </main>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,33 @@
 import React, { FC } from "react"
 
-export const Card: FC = (props) => {
+type CardProps = {
+  iconPath: string
+}
+
+export const Card: FC<CardProps> = (props) => {
   return (
     <article className="w-96 h-52 p-3 md:w-100">
-      <div className="bg-white w-full h-full rounded-lg shadow-md">
-        <img className="" alt="アイコン" />
+      <div className="bg-white w-full h-full p-3 flex rounded-lg shadow-md ">
+        <div className="w-1/5 ">
+          <img
+            className=" w-full aspect-square h- rounded-full border-2 shadow-sm "
+            alt="アイコン"
+            src={props.iconPath}
+          />
+        </div>
+        <div className="w-full pl-2 pt-2 divide-y divide-sky-200">
+          <div className="h-2/5 ">
+            <h3 className="text-gray-600 text-xl">React + TypeScriptの環境構築</h3>
+          </div>
+          <div className="h-2/5 flex items-center">
+            <p className="text-3xl text-sky-500 ">
+              1<span className=" px-1 text-xl">時間</span>30<span className="px-1 text-xl">分</span>
+            </p>
+          </div>
+          <div className="h-1/5">
+            <p className="text-gray-400">@reactdaikukisan</p>
+          </div>
+        </div>
       </div>
     </article>
   )
