cb648a2ac06b310b83059e79550e342db361c347
bmstyojaop <bmbdyojaop@gmail.com>
2023-03-08 22:48:01 +0900

    :sparkles: データの一覧を取得

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,28 +1,10 @@
-import React, { FC } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { FC, useEffect } from "react";
 
 import { Cards } from "../components/Cards";
+import { db } from "../firebase";
 
 const icons = [
-  "/assets/img/testicon1.jpg",
-  "/assets/img/testicon2.jpg",
-  "/assets/img/testicon3.jpg",
-  "/assets/img/testicon4.jpg",
-  "/assets/img/testicon5.jpg",
-  "/assets/img/testicon6.jpg",
-  "/assets/img/testicon7.jpg",
-  "/assets/img/testicon8.jpg",
-  "/assets/img/testicon9.jpg",
-  "/assets/img/testicon10.jpg",
-  "/assets/img/testicon1.jpg",
-  "/assets/img/testicon2.jpg",
-  "/assets/img/testicon3.jpg",
-  "/assets/img/testicon4.jpg",
-  "/assets/img/testicon5.jpg",
-  "/assets/img/testicon6.jpg",
-  "/assets/img/testicon7.jpg",
-  "/assets/img/testicon8.jpg",
-  "/assets/img/testicon9.jpg",
-  "/assets/img/testicon10.jpg",
   "/assets/img/testicon1.jpg",
   "/assets/img/testicon2.jpg",
   "/assets/img/testicon3.jpg",
@@ -36,6 +18,15 @@ const icons = [
 ];
 
 const Home: FC = () => {
+  // マウント時に1度だけ表示したいのでuseEffect
+  useEffect(() => {
+    // async関数を使用する時はuseEffectに渡す関数内でさらに関数を宣言する必要がある。
+    const getStudyLogs = async () => {
+      const data = await getDocs(collection(db, "studylog"));
+    };
+    getStudyLogs();
+  }, []);
+
   return (
     <div className=" w-screen  hidden-scrollbar overflow-hidden flex justify-center">
       <div className="h-full hidden-scrollbar overflow-scroll container flex flex-col items-center pt-4 md:flex-row md:flex-wrap md:items-start md:justify-around md:px-4">
