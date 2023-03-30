4d6f5328fb3b805e528ffad3475b40906baf151d
bmstyojaop <bmbdyojaop@gmail.com>
2023-03-30 10:18:09 +0900

    :lipstick: ラッパーのデザインを修正

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout: FC<layoutProps> = ({ user }) => {
   return (
     <div>
       <Header user={user} />
-      <div className="bg-gray-200 py-10">
+      <div className="bg-gray-200 pt-5 pb-52 h-full w-full fixed overflow-scroll  ">
         <Outlet />
       </div>
     </div>
