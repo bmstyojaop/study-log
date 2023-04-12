1b8a274e4b47cbbae73006e8b4e9e49196616807
bmstyojaop <bmbdyojaop@gmail.com>
2023-04-12 16:53:12 +0900

    ✨ グラフ用のデータ生成処理を追加

--- src/routes/MyPage.tsx
+++ src/routes/MyPage.tsx
@@ -1,8 +1,7 @@
 import type { User } from "@firebase/auth";
-import { AreaChart } from "@tremor/react";
+import { AreaChart, Metric } from "@tremor/react";
 import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 import React, { FC, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import { db } from "../firebase";
 
@@ -16,7 +15,7 @@ type Log = {
   "": number;
 };
 type Result = {
-  date: string;
+  date: Date;
   time: number;
 };
 
@@ -25,34 +24,37 @@ const timeFormatter = (time: number) => {
 };
 
 export const MyPage: FC<MyPageProps> = ({ user }) => {
-  const navigate = useNavigate();
-  const [chartData, setChartData] = useState<Log[] | null>(null);
+  const [chartData, setChartData] = useState<Log[] | null>();
   const studylogRef = collection(db, "studylog");
 
+  const getLatestDate = async (): Promise<Date> => {
+    const latestData = await getDocs(
+      query(
+        studylogRef,
+        where("author.id", "==", user?.uid),
+        orderBy("createdAt", "desc"),
+        limit(1)
+      )
+    );
+    const latestDate: Date = await latestData.docs[0].data().createdAt.toDate();
+    return latestDate;
+  };
+
+  const findThirtyDaysAgo = (date: Date): Date => {
+    const thirtyDaysAgo: Date = new Date(date.getTime() - 30 * 24 * 60 * 60 * 1000);
+    thirtyDaysAgo.setHours(0, 0, 0, 0);
+    return thirtyDaysAgo;
+  };
+
+  const dateFormatter = (date: Date) => {
+    return `${date.getMonth() + 1}/${date.getDate()}`;
+  };
+
   useEffect(() => {
-    if (!user) {
-      navigate("/login");
-      return;
-    }
-
     const getMyLogs = async () => {
-      // 一番最新のデータの日付を取得
-      const latestData = await getDocs(
-        query(
-          studylogRef,
-          where("author.id", "==", user?.uid),
-          orderBy("createdAt", "desc"),
-          limit(1)
-        )
-      );
-
-      const latestDate: Date = latestData.docs[0].data().createdAt.toDate();
-
-      // 30日前の0時0分0秒0ミリ秒を設定
-      const thirtyDaysAgo: Date = new Date(latestDate.getTime() - 30 * 24 * 60 * 60 * 1000);
-      thirtyDaysAgo.setHours(0, 0, 0, 0);
-
-      // 最新から30日分のデータを取得
+      // // 一番最新のデータの日付
+      const latestDate: Date = await getLatestDate();
+      const thirtyDaysAgo: Date = findThirtyDaysAgo(latestDate);
       const data = await getDocs(
         query(
           studylogRef,
@@ -63,15 +65,14 @@ export const MyPage: FC<MyPageProps> = ({ user }) => {
       );
 
       const result = data.docs
+        // 日付と時間を取得
         .map((doc) => ({
-          date: `${doc.data().createdAt.toDate().getMonth() + 1}/ ${doc
-            .data()
-            .createdAt.toDate()
-            .getDate()}`,
+          // 0時0分0秒にする
+          date: new Date(doc.data().createdAt.toDate().setHours(0, 0, 0, 0)),
           time: doc.data().time,
         }))
         .reduce((acc: Result[], cur: Result) => {
-          const target = acc.find((item) => item.date === cur.date);
+          const target = acc.find((item) => item.date.getTime() === cur.date.getTime());
           if (target) {
             target.time += cur.time;
           } else {
@@ -80,21 +81,48 @@ export const MyPage: FC<MyPageProps> = ({ user }) => {
           return acc;
         }, []);
 
-      setChartData(
-        result
-          .map((doc) => ({
-            date: `${doc.date}`,
+      const addMissingData = (result: Result[]) => {
+        // 対象日のデータがない場合は、timeが0のデータを追加
+        for (let i = 0; i < 30; i++) {
+          const targetDate = new Date(thirtyDaysAgo.getTime() + i * 24 * 60 * 60 * 1000);
+          const target = result.find((item) => item.date === targetDate);
+          if (!target) {
+            result.push({
+              date: targetDate,
+              time: 0,
+            });
+          }
+        }
+
+        result.sort((a, b) => {
+          if (a.date < b.date) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+
+        setChartData(
+          result.map((doc) => ({
+            date: `${dateFormatter(doc.date)}`,
             "": doc.time,
           }))
-          .reverse()
-      );
+        );
+      };
+      addMissingData(result);
     };
     getMyLogs();
-  }, []);
+    console.log("useEffect/MyPage");
+  }, [user]);
+
+  if (!user) {
+    return <div>マイページを確認するためにはログインが必要です。</div>;
+  }
   return (
     <div className="hidden-scrollbar  flex w-screen justify-center overflow-hidden">
-      <div className="hidden-scrollbar container flex h-screen flex-col items-center overflow-scroll rounded-3xl bg-white pt-4 drop-shadow-md md:flex-row md:flex-wrap md:items-start md:justify-around md:px-4">
-        <div className=" container flex flex-col items-center pt-4 md:flex-row md:flex-wrap md:items-start md:justify-around md:px-4">
+      <div className="hidden-scrollbar container flex flex-col items-center overflow-scroll rounded-3xl bg-white pt-4 drop-shadow-md md:flex-row md:flex-wrap md:items-start md:justify-around md:px-4">
+        <div className=" container flex h-80 flex-col items-center pt-4 md:flex-row md:flex-wrap md:items-start md:justify-around md:px-4">
+          <Metric className=" text-gray-600">学習チャート</Metric>
           {chartData && (
             <AreaChart
               className="h-72 px-5 py-10"
