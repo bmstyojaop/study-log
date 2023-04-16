f2721bfcdbc8804023505ef63ebc93f945c37bb7
bmstyojaop <bmbdyojaop@gmail.com>
2023-04-16 17:22:17 +0900

    firebaseの設定を修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlYJE17hbGvnFZMrlMeUQJrPysCK9yk_8",
-  appId: "1:171079455579:web:7649655a6615c9453b807e",
-  authDomain: "study-log-sample.firebaseapp.com",
-  messagingSenderId: "171079455579",
-  projectId: "study-log-sample",
-  storageBucket: "study-log-sample.appspot.com",
+  apiKey: "AIzaSyAZYnilNeaWBAJu4PyT2YQKlTqLp2wXMPY",
+  appId: "1:59171733717:web:e8d7cabd91f28d4fcd625c",
+  authDomain: "study-log-477aa.firebaseapp.com",
+  messagingSenderId: "59171733717",
+  projectId: "study-log-477aa",
+  storageBucket: "study-log-477aa.appspot.com",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
